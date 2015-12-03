b06609d03d4f61d582277489d852b7256b45a387
George Opritescu <ssscripting@gmail.com>
2015-12-03 18:26:09 +0100

    Handled 2nd point of 1st challenge

--- challenge_1/lib/challenge_1.ex
+++ challenge_1/lib/challenge_1.ex
@@ -1,30 +1,37 @@
 defmodule Santa do
-  defp floor_number(acc, ?(, [h|t]) do
-    floor_number(acc + 1, h, t)
+  defp modify_sum(new_value, position) do
+    if new_value == -1 do
+      IO.puts "negative at position: #{position}"
+    end
+    new_value
   end
 
-  defp floor_number(acc, ?(, []) do
-    acc + 1
+  defp floor_number(acc, position, ?(, [h|t]) do
+    floor_number(modify_sum(acc + 1, position), position + 1, h, t)
   end
 
-  defp floor_number(acc, ?), []) do
-    acc - 1
+  defp floor_number(acc, position, ?(, []) do
+    modify_sum(acc + 1, position)
   end
 
-  defp floor_number(acc, ?), [h|t]) do
-    floor_number(acc - 1, h, t)
+  defp floor_number(acc, position, ?), []) do
+    modify_sum(acc - 1, position)
   end
 
-  defp floor_number(acc, _, []) do
-    acc
+  defp floor_number(acc, position, ?), [h|t]) do
+    floor_number(modify_sum(acc - 1, position), position + 1, h, t)
+  end
+
+  defp floor_number(acc, position, _, []) do
+    modify_sum(acc, position)
   end
 
   def floor_number([h | t]) do
-    floor_number(0, h, t)
+    floor_number(0, 1, h, t)
   end
 
   def basement_position([h | t]) do
-
+    floor_number([h|t])
   end
 
 end
