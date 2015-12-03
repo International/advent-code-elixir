e2a016de6fefabf7c6eec8e3b86573f2255c81a3
George Opritescu <ssscripting@gmail.com>
2015-12-03 17:59:01 +0100

    Added unit tests

--- challenge_1/test/challenge_1_test.exs
+++ challenge_1/test/challenge_1_test.exs
@@ -2,7 +2,28 @@ defmodule Challenge_1Test do
   use ExUnit.Case
   doctest Santa
 
-  test "(()) is 0" do
+  test "(()) and ()() both result in floor 0" do
     assert Santa.floor_number('(())') == 0
+    assert Santa.floor_number('()()') == 0
   end
+
+  test "((( and (()(()( both result in floor 3" do
+    assert Santa.floor_number('(((') == 3
+    assert Santa.floor_number('(()(()(') == 3
+  end
+
+  test "()) and ))( both result in floor -1 (the first basement level)." do
+    assert Santa.floor_number('())') == -1
+    assert Santa.floor_number('))(') == -1
+  end
+
+  test "))) and )())()) both result in floor -3." do
+    assert Santa.floor_number(')))') == -3
+    assert Santa.floor_number(')())())') == -3
+  end
+
+  test "))((((( also results in floor 3." do
+    assert Santa.floor_number('))(((((') == 3
+  end
+
 end
